77b23af6d78c48934d84b499ce8bfd4735b920bb
trufi <yura@gmail.com>
2025-03-14 19:57:05 +0200

    homework

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import pytest
+
+from models import Book, Library
+
+
+@pytest.fixture(scope="session")
+def book() -> Book:
+    harry_potter = Book(name="Harry Potter", name_author="Joanne Rowling")
+    return harry_potter
+
+
+@pytest.fixture(scope="session")
+def library() -> Library:
+    library = Library(name="KSD")
+    return library
---
+++ models.py
@@ -0,0 +1,31 @@
+import uuid
+from typing import Self
+
+
+class Book:
+    def __init__(self, name_author: str, name: str):
+        self.name_author: str = name_author
+        self.name: str = name
+        self.inn: uuid.UUID = uuid.uuid4()
+
+    def __str__(self):
+        return f"<Book {self.name} - {self.inn}>"
+
+
+class Library:
+    def __init__(
+        self,
+        name: str,
+    ):
+        self.name: str = name
+        self.name: name
+        self.list_book: list[Book] = []
+
+    def add_book(self, book: Book):
+        self.list_book.append(book)
+
+    def delete_book(self, book_inn):
+        for book in self.list_book:
+            if book.inn == book_inn:
+                self.list_book.remove(book)
+                break
